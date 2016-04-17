55573c1094467f465572905bdeb2a625fc29655e
Zach Barton <email": null>
2016-04-17 16:37:38 -0500

    Board successfully passes to phones

---
+++ js/messages.js
@@ -0,0 +1,17 @@
+var AC =  {};
+
+AC.Action = {
+  GetBoard: "transfer"
+};
+
+
+/**
+ * A simple helper function to append text to an element
+ */
+var appendTextToElement = function(parent_ele, text) {
+  var ele = document.createElement('DIV');
+  ele.innerHTML = text;
+  parent_ele.appendChild(ele);
+};
+
+
